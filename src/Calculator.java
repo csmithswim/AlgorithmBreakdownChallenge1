b7dff70a81de5f72c532cf40377b0fd7aaf0f54d
Cullen S <cullensmi@gmail.com>
2020-11-10 19:46:21 -0500

    Solved algorithm

---
+++ src/Calculator.java
@@ -0,0 +1,18 @@
+/*
+v1.0 Create a method that takes a string.
+
+v1.1 update the method to return an int and return 0 regardless of what the input was.
+v1. 2 update method to accept a string in the following format "X + Y" (Do not worry about validating that it's that format)
+v1.3 parse the equation into three parts X, Operator, Y
+v1.4 parse X and return X
+v1.5 parse Y and add X and Y. return result.
+v1.6 determine the Operator and apply the correct operation to X and Y return the result.
+ */
+
+//public class Calculator {
+//    public int calculate(String input){
+//
+//        System.out.println(0);
+//        return 0;
+//    }
+//}
--- src/com/csmithswim/Main.java
+++ src/com/csmithswim/Main.java
@@ -1,8 +1,54 @@
 package com.csmithswim;
+import java.util.Arrays;
 
 public class Main {
+    public static int calculate(String input) {
+        String[] result = input.split(" ");
+        String operand = result[1];
+
+        switch (operand) {
+            case "+":
+                Integer sum = Integer.parseInt(result[0]) + Integer.parseInt(result[2]);
+                System.out.println(sum);
+                break;
+
+            case "-":
+                Integer difference = Integer.parseInt(result[0]) - Integer.parseInt(result[2]);
+                System.out.println(difference);
+                break;
+
+            case "/":
+                Integer quotient = Integer.parseInt(result[0]) / Integer.parseInt(result[2]);
+                System.out.println(quotient);
+                break;
+
+            case "*":
+                Integer product = Integer.parseInt(result[0]) * Integer.parseInt(result[2]);
+                System.out.println(product);
+                break;
+        }
+
+
+
+//        if (operand.equals("+")) {
+//            Integer sum = Integer.parseInt(result[0]) + Integer.parseInt(result[2]);
+//            System.out.println(sum);
+//        } else if (operand.equals("-")) {
+//            Integer difference = Integer.parseInt(result[0]) - Integer.parseInt(result[2]);
+//            System.out.println(difference);
+//        } else if (operand.equals("/")) {
+//            Integer quotient = Integer.parseInt(result[0]) - Integer.parseInt(result[2]);
+//            System.out.println(quotient);
+//
+//        } else if (operand.equals("*")) {
+//            Integer product = Integer.parseInt(result[0]) * Integer.parseInt(result[2]);
+//            System.out.println(product);
+//        }
+        return 0;
+    }
 
     public static void main(String[] args) {
-	// write your code here
+
+    calculate("20 * 2");
     }
 }
